1bf7b93628eb04f1a84299c5b299419df6658ffb
zhangsan <zhangsan@itcast.cn>
2019-06-10 09:11:02 +0800

    提交

---
+++ src/cn/itcast/git/FirstClass.java
@@ -0,0 +1,16 @@
+package cn.itcast.git;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
